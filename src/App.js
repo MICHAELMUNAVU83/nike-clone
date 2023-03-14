0e485b53bddbadeaef12b1e3ab0d1b5e2210fafc
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-03-14 20:23:24 +0300

    Finished featured styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import NavBar from "./components/NavBar";
 import AfterNavBar from "./components/AfterNavBar";
+import Home from "./components/Home";
+import FeaturedStyles from "./components/FeaturedStyles";
 function App() {
   return (
     <div className="App">
       <NavBar />
       <AfterNavBar />
+      <Home />
+      <FeaturedStyles />
     </div>
   );
 }
